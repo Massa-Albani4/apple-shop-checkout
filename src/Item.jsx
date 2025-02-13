056acc16658d64daadd4993d35a1ca7244861dc3
Massa-Albani4 <massa.albani0213@gmail.com>
2025-02-13 17:41:29 +0400

    Fix header title font size

--- src/Item.jsx
+++ src/Item.jsx
@@ -8,16 +8,16 @@ function Item() {
         <img src={product} alt="product-image" />
       </div>
       <div className="app__main__checkout__items-container__item__product-info">
-        <div>
-          <div>
-            <p>iPhone 12 Pro</p>
-            <p>Golden</p>
-          </div>
-          <p>$999.00</p>
-        </div>
-        <div className="flex">
+        <div className="app__main__checkout__items-container__item__product-info__product-name-quantity">
+          <p>iPhone 12 Pro</p>
+          <p>Golden</p>
           <p>X 1</p>
-          <button>
+        </div>
+        <div className="app__main__checkout__items-container__item__product-info__price-delete-container">
+          <p className="app__main__checkout__items-container__item__product-info__price">
+            $999.00
+          </p>
+          <button className="app__main__checkout__items-container__item__delete-btn">
             <img src={deleteIcon} alt="delete button" />
           </button>
         </div>
